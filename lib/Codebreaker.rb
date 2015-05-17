36ad9641c2d51af9caa19ff5e1a31fcae25927dc
megapixel23 <megapixel23@ya.ru>
2015-05-17 23:23:15 +0300

    test added

--- lib/Codebreaker.rb
+++ lib/Codebreaker.rb
@@ -3,6 +3,7 @@ require "Codebreaker/version"
 module Codebreaker
   class Game
     attr_reader :code
+    attr_reader :input_code
 
     def initialize
       @code = ''
@@ -12,6 +13,22 @@ module Codebreaker
       4.times do
         @code << rand(1...6).to_s
       end
+    end
+
+    def input_code=(input)
+      unless input.kind_of?(Numeric) || input.kind_of?(String)
+        raise Exception
+      end
+
+      unless input =~ /^[1-6]{4}$/
+        raise Exception
+      end
+
+      @input_code = input.to_s
+    end
+
+    def check_code
+
     end
   end
 end
--- spec/Codebreaker_spec.rb
+++ spec/Codebreaker_spec.rb
@@ -2,32 +2,45 @@ require 'spec_helper'
 
 module Codebreaker
   describe Game do
+    let(:game) do
+      game = Game.new
+      game.start
+      game
+    end
+
     describe '#start' do
       it 'generates secret code' do
-        game = Game.new
-        game.start
         expect(game.instance_variable_get(:@code)).not_to be_empty
       end
 
       it 'saves 4 numbers secret code' do
-        game = Game.new
-        game.start
         expect(game.code.length).to be(4)
       end
 
       it 'saves secret code with numbers from 1 to 6' do
-        game = Game.new
-        game.start
         expect(game.code).to match(/^[1-6]+$/)
       end
 
-      it 'generates random codes' do
-        game1 = Game.new
-        game1.start
-        game2 = Game.new
-        game2.start
+      it 'checks code randomization' do
+        another_game = Game.new
+        another_game.start
 
-        expect(game1.code).not_to eq(game2.code)
+        expect(game.code).not_to eq(another_game.code)
+      end
+    end
+
+    describe '#input_code' do
+      it 'take a guess and inputs code' do
+        game.input_code = '1234'
+
+        expect(game.input_code).not_to be_empty
+      end
+
+      it 'checks user code validation' do
+        expect{game.input_code = {}}.to raise_exception
+        expect{game.input_code = []}.to raise_exception
+        expect{game.input_code = ''}.to raise_exception
+        expect{game.input_code = 'test data'}.to raise_exception
       end
     end
   end
